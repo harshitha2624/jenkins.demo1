fc2a67a242875fc6de15003fd5ccb90a35cb6b6e
harshitha2624 <166125609+harshitha2624@users.noreply.github.com>
2025-03-24 12:54:40 +0530

    java3

---
+++ Cmdline2.java
@@ -0,0 +1,15 @@
+class Cmdline2
+{
+public static void main(String args[])
+{
+
+String s1=args[0];
+String s2=args[1];
+String s3=args[2];
+
+System.out.println("name:"+s1);
+System.out.println("rollno:"+s2);
+System.out.println("Branch:"+s3);
+
+}
+}
