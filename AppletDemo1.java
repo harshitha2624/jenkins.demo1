b8d6b68ff70706bf16f16ed56c2732432c746361
harshitha2624 <166125609+harshitha2624@users.noreply.github.com>
2025-03-25 12:12:12 +0530

    javaapplet

---
+++ AppletDemo1.java
@@ -0,0 +1,9 @@
+import java.applet.*;
+import java.awt.*;
+/*<applet code="AppletDemo1.class" width="300" height="300"></applet>*/
+public class AppletDemo1 extends Applet{
+public void paint(Graphics g)
+{
+g.drawString("hello world",120,180);
+}
+}
